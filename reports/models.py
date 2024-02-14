dff11d9ab2780fbbcb0addc082b45dd54471d0ec
KrimssmirK <krimssmirk003@gmail.com>
2024-02-14 16:46:02 +0800

    modified models

--- reports/models.py
+++ reports/models.py
@@ -6,25 +6,26 @@ def create_choices(model):
     choices = {str(item_name)[0]: str(item_name) for item_name in items_on_db}
     return choices
 
-class VisaType(models.Model):
-    visa_type = models.CharField(max_length=200)
-    
-    def __str__(self):
-        return self.visa_type
-
-
-class Coordinator(models.Model):
-    name = models.CharField(max_length=200)
-    
-    def __str__(self):
-        return self.name
-    
 
 class Report(models.Model):
-    visa_type = models.CharField(max_length=1, choices=create_choices(VisaType), default="")
-    coordinator = models.CharField(max_length=1, choices=create_choices(Coordinator), default="")
+    visa_type = models.CharField(max_length=200)
+    coordinator = models.CharField(max_length=200)
+    no_of_pax = models.IntegerField(default=1, choices=((i,i) for i in range(1, 101)))
     report_date = models.DateTimeField("date reported")
     
     def __str__(self):
         return self.visa_type
     
+    
+class VisaType(models.Model):
+    visa_type = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.visa_type
+    
+    
+class Coordinator(models.Model):
+    name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name
