cdd9a3efbfb748ad90648af79f3aec050e3276e9
KrimssmirK <krimssmirk003@gmail.com>
2024-02-14 13:31:01 +0800

    added Visa model

--- reports/admin.py
+++ reports/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Visa
 
-# Register your models here.
+admin.register(Visa)
--- reports/models.py
+++ reports/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Visa(models.Model):
+    visa_type = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.visa_type
