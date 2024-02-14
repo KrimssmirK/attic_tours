be83bb712af821ef9b80982218967d7689aab513
KrimssmirK <krimssmirk003@gmail.com>
2024-02-14 14:10:42 +0800

    added test case for visa type model

--- reports/tests.py
+++ reports/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import VisaType
 
-# Create your tests here.
+
+class VisaTypeModelTests(TestCase):
+    def test_add_visa_type(self):
+        """
+        add visa type and check if it is added correctly
+        """
+        new_visa_type = VisaType(visa_type="TOURISM")
+        new_visa_type.save()
+        visa_type_on_db = VisaType.objects.first()
+        self.assertEqual(visa_type_on_db, new_visa_type)
+        
