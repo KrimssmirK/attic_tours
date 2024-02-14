2cd1a159843cde226d6fb82d42b245bf87c29122
KrimssmirK <krimssmirk003@gmail.com>
2024-02-14 13:44:56 +0800

    added coordinator model

--- reports/models.py
+++ reports/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 
+
 class VisaType(models.Model):
     visa_type = models.CharField(max_length=200)
     
     def __str__(self):
         return self.visa_type
+
+
+class Coordinator(models.Model):
+    name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name
