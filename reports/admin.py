ac35b8c71afc677c687e65d15b0fca3a28aca3be
KrimssmirK <krimssmirk003@gmail.com>
2024-02-14 14:50:10 +0800

    added report model

--- reports/admin.py
+++ reports/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import VisaType, Coordinator
+from .models import VisaType, Coordinator, Report
 
 admin.site.register(VisaType)
 admin.site.register(Coordinator)
+admin.site.register(Report)
--- reports/models.py
+++ reports/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
 
+def create_choices(model):
+    items_on_db = model.objects.all()
+    choices = {str(item_name)[0]: str(item_name) for item_name in items_on_db}
+    return choices
+
 class VisaType(models.Model):
     visa_type = models.CharField(max_length=200)
     
@@ -13,3 +18,13 @@ class Coordinator(models.Model):
     
     def __str__(self):
         return self.name
+    
+
+class Report(models.Model):
+    visa_type = models.CharField(max_length=1, choices=create_choices(VisaType), default="")
+    coordinator = models.CharField(max_length=1, choices=create_choices(Coordinator), default="")
+    report_date = models.DateTimeField("date reported")
+    
+    def __str__(self):
+        return self.visa_type
+    
