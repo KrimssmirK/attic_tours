7bdf4ca70c59ff8acb5dc65c0cc9a6fd5f855cd6
KrimssmirK <krimssmirk003@gmail.com>
2024-02-15 16:15:35 +0800

    changed view in admin

--- reports/admin.py
+++ reports/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import VisaType, Coordinator, Report
 
+
+class ReportAdmin(admin.ModelAdmin):
+    list_display = ["report_date", "visa_type", "no_of_pax", "coordinator"]
+    list_filter = ["report_date"]
+
 admin.site.register(VisaType)
 admin.site.register(Coordinator)
-admin.site.register(Report)
+admin.site.register(Report, ReportAdmin)
