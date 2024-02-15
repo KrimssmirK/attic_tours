c1331ac350346915c7542efe93e8ec2adea645f1
KrimssmirK <krimssmirk003@gmail.com>
2024-02-15 17:49:33 +0800

    added foreign key

--- reports/models.py
+++ reports/models.py
@@ -1,16 +1,6 @@
 from django.db import models
 
 
-class Report(models.Model):
-    visa_type = models.CharField(max_length=200)
-    coordinator = models.CharField(max_length=200)
-    no_of_pax = models.IntegerField(default=1, choices=((i,i) for i in range(1, 101)))
-    report_date = models.DateTimeField("date reported")
-    
-    def __str__(self):
-        return self.visa_type
-    
-    
 class VisaType(models.Model):
     visa_type = models.CharField(max_length=200)
     
@@ -23,3 +13,14 @@ class Coordinator(models.Model):
     
     def __str__(self):
         return self.name
+
+
+class Report(models.Model):
+    visa_type = models.ForeignKey(VisaType,on_delete=models.CASCADE)
+    coordinator = models.ForeignKey(Coordinator,on_delete=models.CASCADE)
+    no_of_pax = models.IntegerField(default=1, choices=((i,i) for i in range(1, 101)))
+    report_date = models.DateTimeField("date reported")
+
+    
+    
+
