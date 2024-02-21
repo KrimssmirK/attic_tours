02f00c879455611b4fb1015a1efea8e5664108b7
KrimssmirK <krimssmirk003@gmail.com>
2024-02-21 15:00:46 +0800

    added functionalities

--- reports/templatetags/report_extras.py
+++ reports/templatetags/report_extras.py
@@ -2,13 +2,25 @@ from django import template
 
 register = template.Library()
 
+@register.filter(name="has_reports")
+def has_reports(dict):
+    return len(dict) != 0
+
 
 @register.filter(name="lookup")
 def lookup(dict, key):
     if key in dict:
         return dict[key]
-    else: 
+    else:
         return 0
+    
+@register.filter(name="is_exist")
+def is_exist(dict, key):
+    if key in dict:
+        return True
+    else:
+        return False
+    
 
 
 @register.filter(name="get_total")
