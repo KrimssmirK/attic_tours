11560cc5326c923e5917d6b729c4c43404d5004e
KrimssmirK <krimssmirk003@gmail.com>
2024-02-21 13:14:09 +0800

    removed logic for getting current reports

--- reports/views.py
+++ reports/views.py
@@ -1,22 +1,12 @@
 from django.shortcuts import render
-from django.utils import timezone
 from .models import Report, VisaType, Coordinator
 
 
 
 def index(request):
-    reports = Report.objects.filter(report_date__date=timezone.now().date())
-    visa_types = VisaType.objects.all()
-    
-    # sum up each visa types
-    reports_dicts = dict()
-    for report in reports:
-        if report.visa_type in reports_dicts:
-            reports_dicts[report.visa_type] += report.no_of_pax
-        else:
-            reports_dicts[report.visa_type] = report.no_of_pax
-            
-    context = {"reports": reports_dicts, "visa_types": visa_types}
+    reports = Report.get_current_reports()
+    visa_types = VisaType.objects.all()        
+    context = {"reports": reports, "visa_types": visa_types}
     return render(request, "reports/index.html", context)
 
 
