d2998bd003e5fc9b6613c0a320abaf101f070b9f
KrimssmirK <krimssmirk003@gmail.com>
2024-02-29 15:27:33 +0800

    refactor

--- reports/views.py
+++ reports/views.py
@@ -1,15 +1,13 @@
 from django.shortcuts import render
-from django.http import JsonResponse
-from .models import Report, VisaType, Coordinator
+from .models import Report, VisaType
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
 
 
-
 def index(request):
-    reports = Report.get_current_reports()
+    current_reports = Report.get_current_reports()
     visa_types = VisaType.objects.all()        
-    context = {"reports": reports, "visa_types": visa_types}
+    context = {"reports": current_reports, "visa_types": visa_types}
     return render(request, "reports/index.html", context)
 
 
