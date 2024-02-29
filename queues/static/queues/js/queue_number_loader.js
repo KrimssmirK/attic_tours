8afb56aa476b556c1363a820d512003ffd03e9d9
KrimssmirK <krimssmirk003@gmail.com>
2024-02-29 21:18:25 +0800

    add: ring function before calling customers

--- queues/static/queues/js/queue_number_loader.js
+++ queues/static/queues/js/queue_number_loader.js
@@ -5,23 +5,26 @@ const state = {
   }
 }
 
-function updateQueueNumber(endpoint, interval, DomID, hasCallVoice) {
+function updateQueueNumber(endpoint, interval, DomID, hasCallVoice, audioID) {
   var intervalId = window.setInterval(function () {
-    getCurrentQueueNumber(endpoint)
+    getCurrentQueueNumber(endpoint, audioID)
   }, interval)
 
-  function getCurrentQueueNumber(endpoint) {
+  function getCurrentQueueNumber(endpoint, audioID) {
     const xhttp = new XMLHttpRequest()
     xhttp.onload = function () {
       // What to do when the response is ready
       const received_data = JSON.parse(xhttp.responseText)
       state.received_data = received_data
-  
+
       // update UI
       if (!_.isEqual(received_data, state.previous_data)) {
         updateTargetDOM(DomID)
         if (hasCallVoice) {
-          callCustomer()
+          // call ring
+          ring(audioID)
+          // call customer
+          // callCustomer()
         }
       }
       state.previous_data = received_data
@@ -35,13 +38,19 @@ function updateQueueNumber(endpoint, interval, DomID, hasCallVoice) {
     targetDOM.innerText = state.received_data.current_queue_number
   }
 
-  function callCustomer() {
-    const templateSpeach = "japan visa applicant number"
-    let utterance = new SpeechSynthesisUtterance(templateSpeach + state.received_data.current_queue_number)
-    speechSynthesis.speak(utterance)
+  function ring(audioID) {
+    const targetAudio = document.getElementById(audioID)
+    targetAudio.play()
+    targetAudio.setAttribute("onended", "callCustomer()")
   }
-}
 
+  
+}
+function callCustomer() {
+  const templateSpeach = "japan visa applicant number"
+  let utterance = new SpeechSynthesisUtterance(templateSpeach + state.received_data.current_queue_number)
+  speechSynthesis.speak(utterance)
+}
 
 
 
