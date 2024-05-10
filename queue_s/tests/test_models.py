a94575d1e0d12d08ead8a06581236b037d4f9112
KrimssmirK <krimssmirk003@gmail.com>
2024-05-10 12:54:46 +0800

    delete

--- queue_s/tests/test_models.py
+++
@@ -1,3 +0,0 @@
-from django.test import TestCase
-from django.utils import timezone
-from queues.models import Queue, Service
--- queue_s/views/api_like_views/branch_api.py
+++
@@ -1,7 +0,0 @@
-from queues.models import Branch
-from django.http import JsonResponse
-
-
-def get_branches(request):
-    services = Branch.objects.all().values()
-    return JsonResponse(list(services), safe=False, status=200)
