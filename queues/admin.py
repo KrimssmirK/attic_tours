12006316b9800578c4290d1f3cb817f418aba073
KrimssmirK <krimssmirk003@gmail.com>
2024-05-14 12:42:24 +0800

    add readability in admin page

--- queues/admin.py
+++ queues/admin.py
@@ -1,12 +1,41 @@
 from django.contrib import admin
-from queues.models import Branch, Service, Report, Window, Queue
+from queues.models import (
+    Branch, Service, Report, Window, Queue, PrefQueue, Newsfeed, Feedback, QueueSettingStatus
+    )
 
+
+class BranchAdmin(admin.ModelAdmin):
+    list_display = ["name", "mobile_no", "landline_no"]
+    
 class ReportAdmin(admin.ModelAdmin):
     list_display = ["branch", "by", "service", "pax", "date"]
     list_filter = ["branch", "date"]
 
-admin.site.register(Branch)
-admin.site.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ["name", "price"]
+    
+class PrefQueueAdmin(admin.ModelAdmin):
+    list_display = ["branch", "service"]
+    list_filter = ["branch"]
+    
+class NewsfeedAdmin(admin.ModelAdmin):
+    list_display = ["branch", "text"]
+    list_filter = ["branch"]
+
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ["branch", "title", "date"]
+    list_filter = ["branch", "date"]
+    
+class QueueSettingStatusAdmin(admin.ModelAdmin):
+    list_display = ["branch", "change"]
+
+
+admin.site.register(Branch, BranchAdmin)
+admin.site.register(Service, ServiceAdmin)
 admin.site.register(Report, ReportAdmin)
 admin.site.register(Window)
 admin.site.register(Queue)
+admin.site.register(PrefQueue, PrefQueueAdmin)
+admin.site.register(Newsfeed, NewsfeedAdmin)
+admin.site.register(Feedback, FeedbackAdmin)
+admin.site.register(QueueSettingStatus, QueueSettingStatusAdmin)
