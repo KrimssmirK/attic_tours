e27ab7d89dcb0ed608970f3f4610c51a32c29efc
KrimssmirK <krimssmirk003@gmail.com>
2024-05-20 15:30:25 +0800

    feat: add applicants queue ui for main branch

--- queues/views/views_applicant_queue.py
+++ queues/views/views_applicant_queue.py
@@ -4,13 +4,25 @@ from queues.models import Branch
 
 def applicant_queue(request, branch_id):
     logged_in_branch = Branch.objects.get(pk=branch_id)
-    return render(
-        request,
-        "queues/applicant/index.html",
-        {
-            "branch_id": branch_id,
-            "branch_name": logged_in_branch.name,
-            "key_shift": 4,
-            "key_scale": 2,
-        },
-    )
+    if logged_in_branch.name == "MAIN OFFICE":
+        return render(
+            request,
+            "queues/applicant/index_for_main.html",
+            {
+                "branch_id": branch_id,
+                # "branch_name": logged_in_branch.name,
+                # "key_shift": 4,
+                # "key_scale": 2,
+            },
+        )
+    else:
+        return render(
+            request,
+            "queues/applicant/index.html",
+            {
+                "branch_id": branch_id,
+                # "branch_name": logged_in_branch.name,
+                # "key_shift": 4,
+                # "key_scale": 2,
+            },
+        )
